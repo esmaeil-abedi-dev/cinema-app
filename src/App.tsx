7565b67902566ab983e2a02d6cecf61b78cfa9da
Esmaeil Abedi <esmaeil.abedi@snapp.cab>
2021-09-28 19:31:50 +0330

    - config deploy to staging with tag - add v1 for develop env in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Develop ENV
+          Learn React - Develop ENV v1
         </a>
       </header>
     </div>
